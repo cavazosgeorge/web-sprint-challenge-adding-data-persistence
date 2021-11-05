d2a896bd7223f8e62e1037bce39e498ff2ad5605
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 18:40:32 -0400

    Task Model: getTasks query built

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,25 @@
-// build your `Task` model here
+const db = require("../../data/dbConfig");
+
+async function getTasks() {
+  const allTasks = await db("tasks").leftOuterJoin(
+    "projects",
+    "tasks.project_id",
+    "projects.project_id"
+  );
+
+  const tasks = allTasks.map((tasks) => {
+    const formattedTasks = {
+      task_id: tasks.task_id,
+      task_notes: tasks.task_notes,
+      task_description: tasks.task_description,
+      task_completed: tasks.task_completed === 0 ? false : true,
+      project_id: tasks.project_id,
+      project_name: tasks.project_name,
+      project_description: tasks.project_description,
+    };
+    return formattedTasks;
+  });
+  return tasks;
+}
+
+module.exports = { getTasks };
