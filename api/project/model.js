51c566c6727e2b4bec06945c599fa416dc5fc2c0
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 17:58:03 -0400

    Project Model: getProjects query built

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,20 @@
-// build your `Project` model here
+const db = require("../../data/dbConfig");
+
+async function getProjects() {
+  const allProjects = await db("projects");
+
+  const projects = allProjects.map((project) => {
+    const projectTest = {
+      project_id: project.project_id,
+      project_name: project.project_name,
+      project_description: project.project_description,
+      project_completed: project.project_completed === 0 ? false : true,
+    };
+    return projectTest;
+  });
+  return projects;
+}
+
+module.exports = {
+  getProjects,
+};
