1e03dc696cf9d6989af46c6a9bd25b6d6939c683
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 18:58:58 -0400

    Migration: Tasks table created

--- data/migrations/20211105214420_first-migration.js
+++ data/migrations/20211105214420_first-migration.js
@@ -10,6 +10,20 @@ exports.up = async function (knex) {
       table.increments("resource_id");
       table.string("resouce_name").notNullable().unique();
       table.string("resource_description");
+    })
+    .createTable("tasks", (table) => {
+      table.increments("task_id");
+      table.string("task_description").notNullable();
+      table.string("task_notes");
+      table.boolean("task_completed").defaultTo(false);
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
 };
 
