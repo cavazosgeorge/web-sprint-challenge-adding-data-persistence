18941178b64cc8562201edf7b78846663d450228
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 18:01:06 -0400

    Project Router: GET / endpoint built

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,18 @@
-// build your `/api/projects` router here
+const express = require("express");
+const projectsModel = require("./model");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  projectsModel
+    .getProjects()
+    .then((projects) => {
+      res.status(200).json({
+        success: true,
+        data: projects,
+      });
+    })
+    .catch(next);
+});
+
+module.exports = router;
