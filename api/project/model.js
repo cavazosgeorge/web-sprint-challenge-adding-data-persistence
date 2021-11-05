787c39e7179e404a07696a76ed694ffaaac08ff6
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 18:09:26 -0400

    Project Model: getByProjectId query built

--- api/project/model.js
+++ api/project/model.js
@@ -15,6 +15,21 @@ async function getProjects() {
   return projects;
 }
 
+async function getByProjectId(project_id) {
+  const singleProject = await db("projects")
+    .where("project_id", project_id)
+    .first();
+
+  const formattedProject = {
+    project_id: singleProject.project_id,
+    project_name: singleProject.project_name,
+    project_description: singleProject.project_description,
+    project_completed: singleProject.project_completed === 0 ? false : true,
+  };
+  return formattedProject;
+}
+
 module.exports = {
   getProjects,
+  getByProjectId,
 };
