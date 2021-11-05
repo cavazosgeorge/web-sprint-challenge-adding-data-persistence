c616d6076aaf3788598a785976e5d83952bd6f4a
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 18:27:51 -0400

    Resources Model: createdResource query built

--- api/resource/model.js
+++ api/resource/model.js
@@ -4,6 +4,12 @@ function getResources() {
   return db("resources");
 }
 
+async function createResource(resource) {
+  const [resource_id] = await db("resources").insert(resource);
+  return db("resources").where({ resource_id }).first();
+}
+
 module.exports = {
   getResources,
+  createResource,
 };
