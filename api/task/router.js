33e7342423d904a8b33942fc16f9bf963ee94234
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 18:46:33 -0400

    Task Router: File structure created

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,10 @@
-// build your `/api/tasks` router here
+const express = require("express");
+const Tasks = require("./model");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {});
+
+router.post("/", (req, res, next) => {});
+
+module.exports = router;
