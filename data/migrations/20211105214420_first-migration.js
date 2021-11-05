7616233472e265ae97ab1c618f75a54547a6dfdd
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 18:55:15 -0400

    Migration: projects, resources tables created

--- data/migrations/20211105214420_first-migration.js
+++ data/migrations/20211105214420_first-migration.js
@@ -1,8 +1,18 @@
-
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema
+    .createTable("projects", (table) => {
+      table.increments("project_id");
+      table.string("project_name").notNullable();
+      table.string("project_description");
+      table.boolean("project_completed").defaultTo(false);
+    })
+    .createTable("resources", (table) => {
+      table.increments("resource_id");
+      table.string("resouce_name").notNullable().unique();
+      table.string("resource_description");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("projects");
 };
