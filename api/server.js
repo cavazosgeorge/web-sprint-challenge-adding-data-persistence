177a0800a716073bfbfd9b52ef4dc04716b319b3
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-05 17:40:55 -0400

    Server file built

--- api/server.js
+++ api/server.js
@@ -1,1 +1,24 @@
-// build your server here and require it from index.js
+const express = require("express");
+const helmet = require("helmet");
+
+const projectRouter = require("./project/router");
+const taskRouter = require("./task/router");
+const resourceRouter = require("./resource/router");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use("/api/resources", resourceRouter);
+server.use("/api/projects", projectRouter);
+server.use("/api/tasks", taskRouter);
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    succeess: false,
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
